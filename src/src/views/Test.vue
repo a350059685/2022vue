3706890a29bee042f450144508290806d7ede5da
shenhaidalizi <1920858503@qq.com>
2022-06-08 10:11:42 +0800

    更新了封面贴图和头像

---
+++ src/src/views/Test.vue
@@ -0,0 +1,106 @@
+<template>
+  <!--div class="example">
+    <v-contextmenu ref="contextmenu" :theme="theme">
+      <v-contextmenu-item @click="handleClick">删除</v-contextmenu-item>
+      <v-contextmenu-item @click="handleClick">权限设置</v-contextmenu-item>
+
+      <v-contextmenu-item divider></v-contextmenu-item>
+
+      <v-contextmenu-submenu @submenu-show="handleSubmenuShow" title="子菜单">
+        <v-contextmenu-item @click="handleClick">菜单3</v-contextmenu-item>
+
+        <v-contextmenu-item divider></v-contextmenu-item>
+
+        <v-contextmenu-submenu title="子菜单">
+          <v-contextmenu-item @click="handleClick">菜单5</v-contextmenu-item>
+        </v-contextmenu-submenu>
+
+        <v-contextmenu-item @click="handleClick">菜单4</v-contextmenu-item>
+
+        <v-contextmenu-item :auto-hide="false">不自动关闭1</v-contextmenu-item>
+        <v-contextmenu-item :auto-hide="false">不自动关闭2</v-contextmenu-item>
+      </v-contextmenu-submenu>
+    </v-contextmenu>
+
+    <div :class="['box', theme]" @contextmenu.prevent="" v-contextmenu:contextmenu>
+      右键点击此区域
+    </div>
+  </div-->
+  <div>
+    <div class="hoverTest">
+
+    </div>
+    <a-popconfirm
+      title="Are you sure delete this task?"
+      ok-text="Yes"
+      cancel-text="No"
+      @confirm="confirm"
+      @cancel="cancel"
+    >
+      <a href="#">Delete</a>
+    </a-popconfirm>
+    <div>
+      <router-link to="/">上一页</router-link>
+    </div>
+    <div id="app">
+      {{ info }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default {
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    Vue.axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  },
+  methods: {
+    confirm(e) {
+      console.log(e);
+      this.$message.success('Click on Yes');
+    },
+    cancel(e) {
+      console.log(e);
+      this.$message.error('Click on No');
+    },
+    
+  },
+}; 
+/*
+  export default {
+    name: 'Submenu',
+    props: {
+      theme: String,
+    },
+    methods: {
+      handleClick (vm, event) {
+        console.log(vm, event)
+      },
+      handleSubmenuShow (vm, placement) {
+        console.log(vm, placement)
+      },
+    },
+  }*/
+</script>
+
+<style scoped>
+  .box {
+    width: 100%;
+  }
+  .hoverTest{
+    min-width:100px;
+    min-height:100px;
+    background-color:red;
+  }
+
+  .hoverTest.hover{
+    background-color:aqua;
+  }
+</style>
---
+++ src/src/views/Welcome.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="welcome">
+    <vue-particles
+        color="#000"
+        :particleOpacity="0.1"
+        :particlesNumber="40"
+        shapeType="circle"
+        :particleSize="6"
+        linesColor="#000"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.1"
+        :linesDistance="180"
+        :moveSpeed="2"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="lizi"
+        style="z-index:-10"
+      >
+      </vue-particles>
+
+    <a-row>
+        <a-col :span="12">
+            <a-row style="margin-top:35%;margin-bottom:20px"><b style="font-size:50px">金刚石文档</b></a-row>
+            <a-row>
+                <a-button size="large" @click="toRegister" style="width:100px;margin-right:20px">注册</a-button>
+                <a-button size="large" @click="toLogin" style="width:100px">登录</a-button>
+            </a-row>
+            
+        </a-col>
+        <a-col :span="12">
+            
+        </a-col>
+    </a-row>
+  </div>
+</template>
+
+<style>
+  .welcome {
+  background: url("../assets/cover.png");
+  background-size: 100% 100% ;
+  background-attachment: fixed;
+}
+
+#particles-js{
+  width: 100%;
+  height: calc(100% - 100px);
+  position: absolute;    
+}
+</style>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'Welcome',
+    components: {
+    },
+    created(){
+        this.$store.state.showNav = false
+    },
+    destroyed(){
+        this.$store.state.showNav = true
+    },
+    methods: {
+    toRegister() {
+      this.current = "register";
+      this.$router.push({ path: "/register" });
+    },
+    toLogin(){
+      this.current='login';
+      this.$router.push({path:"/login"});
+    },
+  },
+}
+</script>
