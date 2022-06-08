da0775a073838fdd43b006246c9253fc925f97cc
shenhaidalizi <1920858503@qq.com>
2022-06-08 13:32:53 +0800

    update some details

--- src/src/views/Welcome.vue
+++ src/src/views/Welcome.vue
@@ -25,8 +25,8 @@
         <a-col :span="12">
             <a-row style="margin-top:35%;margin-bottom:20px"><b style="font-size:50px">金刚石文档</b></a-row>
             <a-row>
-                <a-button size="large" @click="toRegister" style="width:100px;margin-right:20px">注册</a-button>
-                <a-button size="large" @click="toLogin" style="width:100px">登录</a-button>
+                <a-button size="large" @click="toRegister" style="width:100px;margin-right:20px;background-color: #e7e7e7; color: black;">注册</a-button>
+                <a-button size="large" @click="toLogin" style="width:100px;background-color: #e7e7e7; color: black;">登录</a-button>
             </a-row>
             
         </a-col>
